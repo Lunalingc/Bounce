0a182e27b0d9effbab58e7cabc9fda00866bfc50
Lunalingc <luna.chinon@gmail.com>
2021-09-09 18:49:06 +0000

    donderdag 9 september deel 2 (werkt niet)

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var x = 50;
 var y = 50;
 var speedX = 2;
 var speedY = 5;
+var positionX = [50,40,30,10];
+var positionY = [50,40,30,10];
 
 
 /**
@@ -33,20 +35,24 @@ function draw() {
   x = speedX + x; 
   y = speedY + y;
 
-  if (y === 680){
+  if (y > 680 || y < 40) {
     speedY = speedY * -1;
   }
 
-  if ( y === 40){
-    speedY = speedY * -1;
-  }
-
-  if ( x === 40) {
-    speedX = speedX * -1;
-  }
-
-  if ( x === 1240){
+  if (x > 1240 || x < 40) {
     speedX = speedX * -1;
   }
  
+  for (var i = 0; i < positionX.length; i++) {
+
+    var x = positionX[i];
+    var y = positionY[i];
+    fill(100,100,255);
+    ellipse(x, y, 40,40);
+
+    x = x + speedX;
+    y = y + speedY;
+
+  }
+
 }
